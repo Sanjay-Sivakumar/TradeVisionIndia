2fb7679469074531d5f338c83a2a8c58debeb1ab
Sandykum <ssannthoshkumar@gmail.com>
2021-07-16 21:04:15 +0530

    Notification

---
+++ app/src/main/java/com/example/notificationappmsg/RemainderBroadcast.java
@@ -0,0 +1,23 @@
+package com.example.notificationappmsg;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class RemainderBroadcast extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(context, "Notification")
+                .setSmallIcon(R.drawable.add_equipment)
+                .setContentTitle("Remind me")
+                .setContentText("Hey this is soft remainder....")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager=NotificationManagerCompat.from(context);
+
+        notificationManager.notify(200, builder.build());
+    }
+}
